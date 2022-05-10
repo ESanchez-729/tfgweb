77aeca2f3b67521ce105b8cf320d4720e353ce5e
EdgarSanchez <edgar.bolea@gmail.com>
2022-05-10 23:59:54 +0200

    Tab bar y mejoras en el search field

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchView from "../views/SearchView";
 import Skeleton from './Skeleton';
 
 const Router = () => {
@@ -7,7 +8,7 @@ const Router = () => {
 
         <BrowserRouter>
             <Routes>
-                <Route path="/search-page" element={<Skeleton contenido={<h1>Search</h1>}/>}/>
+                <Route path="/search-page" element={<Skeleton contenido={<SearchView/>}/>}/>
                 <Route path="*" element={<Skeleton contenido={<div></div>}/>}/>
             </Routes>
         </BrowserRouter>
